15028dc229d577ed2dd04d571e8a75867e47bd52
xxzz1126 <491336135@qq.com>
2018-07-02 11:33:14 +0800

    Update FMDatabasePool.m

--- FMDatabasePool.m
+++ FMDatabasePool.m
@@ -2,8 +2,8 @@
 //  FMDatabasePool.m
 //  fmdb
 //
-//  Created by August Mueller on 6/22/11.
-//  Copyright 2011 Flying Meat Inc. All rights reserved.
+//  Created by Weikuan Chen on 15/30/05.
+//  Copyright 2015 Flying Weikuan All rights reserved.
 //
 
 #import "FMDatabasePool.h"
